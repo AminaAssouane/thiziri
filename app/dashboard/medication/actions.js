0d9dc9f78c6769d8543c0396223bfb8397b3c165
Amina <minassouane@gmail.com>
2026-07-12 15:41:13 +0100

    feat: implement adding a medication

--- app/dashboard/medication/actions.js
+++ app/dashboard/medication/actions.js
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function getMedications() {
   const session = await auth();
@@ -14,4 +16,32 @@ export async function getMedications() {
   });
 
   return medications;
+}
+
+export async function addMedication(formData) {
+  const session = await auth();
+  if (!session?.user?.id) throw new Error("Not authenticated");
+
+  const userId = session.user.id;
+  const name = formData.get("name");
+  const dosage = formData.get("dosage");
+  const frequency = formData.get("frequency");
+  const startDate = new Date(formData.get("startDate"));
+
+  const endDateRaw = formData.get("endDate");
+  const endDate = endDateRaw ? new Date(formData.get("endDate")) : null;
+
+  const notes = formData.get("notes") || null;
+
+  try {
+    const medication = await prisma.medication.create({
+      data: { userId, name, dosage, frequency, startDate, endDate, notes },
+    });
+  } catch (error) {
+    console.error("Could not add medication. Error : ", error);
+    return { error: "Could not add medication." };
+  }
+
+  revalidatePath("/dashboard/medication");
+  redirect("/dashboard/medication");
 }
